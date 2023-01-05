7134f926dfd55f6dea490578765d55a193cbfef3
Olena Kameneva <lenakr0910@gmail.com>
2023-01-05 23:27:39 +0200

    Profile+styled

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,34 +1,33 @@
 import PropTypes from 'prop-types';
-import { Frame } from './Profile.styled';
+import { Frame, CardUser, Avatar, UserName, UserInfo, Stats, StatsItem, StatsLabel, StatsInfo } from './Profile.styled';
 
 export const Profile = ({ username, tag, location, avatar, followers, views, likes }) => {
     return (
         <Frame>
-  <div className="description">
-    <img
+  <CardUser>
+    <Avatar
       src={avatar}
       alt={username}
-      className="avatar"
     />
-    <p className="name">{username}</p>
-    <p className="tag">@{tag}</p>
-    <p className="location">{location}</p>
-  </div>
+    <UserName>{username}</UserName>
+    <UserInfo>@{tag}</UserInfo>
+    <UserInfo>{location}</UserInfo>
+  </CardUser>
 
-  <ul className="stats">
-    <li>
-      <span className="label">Followers</span>
-      <span className="quantity">{followers}</span>
-    </li>
-    <li>
-      <span className="label">Views</span>
-      <span className="quantity">{views}</span>
-    </li>
-    <li>
-      <span className="label">Likes</span>
-      <span className="quantity">{likes}</span>
-    </li>
-  </ul>
+  <Stats>
+    <StatsItem>
+      <StatsLabel>Followers</StatsLabel>
+      <StatsInfo>{followers}</StatsInfo>
+    </StatsItem>
+    <StatsItem>
+      <StatsLabel>Views</StatsLabel>
+      <StatsInfo>{views}</StatsInfo>
+    </StatsItem>
+    <StatsItem>
+      <StatsLabel>Likes</StatsLabel>
+      <StatsInfo>{likes}</StatsInfo>
+    </StatsItem>
+  </Stats>
 </Frame>
     )
 }
--- src/components/Profile/Profile.styled.jsx
+++ src/components/Profile/Profile.styled.jsx
@@ -2,7 +2,62 @@ import styled from '@emotion/styled';
 
 export const Frame = styled.div`
 text-align: center;
-border: 2 px solid red;
-`
+margin: auto;
+border-radius: 10px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 256, 0.5);
+  max-width: 300px;
+  margin-top: 30px;
+`;
 
+export const CardUser = styled.div`
+  margin-bottom: 15px;
+  padding: 10px;
+`;
 
+export const Avatar = styled.img`
+     margin: auto;
+  width: 130px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1);
+  border-radius: 50%;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+export const UserName = styled.p`
+  font-weight: 650;
+  font-size: 20px;
+  margin-bottom: 10px;
+`;
+
+export const UserInfo = styled.p`
+    color: grey;
+    margin-bottom: 10px;
+     font-size: 16px;
+`;
+
+export const Stats = styled.ul`
+    display: flex;
+    justify-content: space-around;
+`;
+
+export const StatsItem = styled.li`
+    display: flex;
+    flex-direction: column;
+     gap: 5px;
+     justify-content: center;
+      background: #e7e7f5;
+      border: 1px #c5c5f5 solid;
+     width: 100%;
+     height: 100px;
+     text-align: center;
+`;
+
+export const StatsLabel = styled.span`
+    color: grey;
+     font-size: 16px;
+`;
+
+export const StatsInfo = styled.span`
+    font-weight: 650;
+  font-size: 16px;
+`;
